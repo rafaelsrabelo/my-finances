951d040a7720c2164bb4beb7f0742f55e7412668
Rafael Rabelo <rafaelrabelo1610@gmail.com>
2022-06-14 22:42:22 -0300

    adicionando miragejs

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,26 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { createServer } from 'miragejs'
 import App from './App'
+
+createServer({
+  routes() {
+    this.namespace = 'api';
+
+    this.get('/transactions', () => {
+      return [
+        {
+          id: 1,
+          title: 'Transactions 1',
+          amount: 400,
+          type: 'deposit',
+          category: 'food',
+          createdAt: new Date()
+        }
+      ]
+    })
+  }
+})
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
