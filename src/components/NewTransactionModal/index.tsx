50387e20e4da745baa0c4cda8fb9930ea343ceac
Rafael Rabelo <rafaelrabelo1610@gmail.com>
2022-06-15 20:37:41 -0300

    criando botoes de tipo

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -1,4 +1,7 @@
 import Modal from 'react-modal'
+import { Container, TransactionTypeContainer } from './styles'
+import { XSquare } from 'phosphor-react'
+import { ArrowCircleDown, ArrowCircleUp, CurrencyDollarSimple } from 'phosphor-react'
 
 interface NewTransactionModalProps {
   isOpen: boolean;
@@ -10,8 +13,26 @@ export function NewTransactionModal({ isOpen, onRequestClose }: NewTransactionMo
     <Modal
       isOpen={isOpen}
       onRequestClose={onRequestClose}
+      overlayClassName="react-modal-overlay"
+      className="react-modal-content"
     >
-      <h2>Cadastrar</h2>
+      <button type="button" onClick={onRequestClose} className="react-modal-close">
+        <XSquare size={32} color="#000" weight="duotone" />
+      </button>
+      <Container>
+        <h2>Cadastrar transação</h2>
+
+        <input type="text" placeholder="Título" />
+        <input type="number" placeholder="Valor" />
+
+        <TransactionTypeContainer>
+          <button type="button"><ArrowCircleUp size={32} color="#53ee5e" weight="duotone" alt="Entrada" /><span>Entrada</span></button>
+          <button type="button"><ArrowCircleDown size={32} color="#ee5353" weight="duotone" alt="Saída" /><span>Saída</span></button>
+        </TransactionTypeContainer>
+
+        <input placeholder="Categoria" />
+        <button type="submit">Cadastrar</button>
+      </Container>
     </Modal>
   )
 }
