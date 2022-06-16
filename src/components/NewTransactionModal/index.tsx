284ed0459d7c3f5b9f97e285756c3e932e6beae6
Rafael Rabelo <rafaelrabelo1610@gmail.com>
2022-06-15 21:36:50 -0300

    inserindo transacao na api

--- src/components/NewTransactionModal/index.tsx
+++ src/components/NewTransactionModal/index.tsx
@@ -2,7 +2,8 @@ import Modal from 'react-modal'
 import { Container, TransactionTypeContainer, RadioBox } from './styles'
 import { XSquare } from 'phosphor-react'
 import { ArrowCircleDown, ArrowCircleUp, CurrencyDollarSimple } from 'phosphor-react'
-import { useState } from 'react';
+import { FormEvent, useState } from 'react';
+import { api } from '../../services/api';
 
 interface NewTransactionModalProps {
   isOpen: boolean;
@@ -10,7 +11,24 @@ interface NewTransactionModalProps {
 }
 
 export function NewTransactionModal({ isOpen, onRequestClose }: NewTransactionModalProps) {
+  const [title, setTitle] = useState('');
+  const [value, setValue] = useState(0);
+  const [category, setCategory] = useState('');
   const [type, setType] = useState('deposit');
+
+  function handleCreateNewTransaction(event: FormEvent) {
+    event.preventDefault()
+
+    const data = ({
+      title,
+      value,
+      category,
+      type
+    });
+
+    api.post('/transactions', data)
+  }
+
   return (
     <Modal
       isOpen={isOpen}
@@ -21,11 +39,11 @@ export function NewTransactionModal({ isOpen, onRequestClose }: NewTransactionMo
       <button type="button" onClick={onRequestClose} className="react-modal-close">
         <XSquare size={32} color="#000" weight="duotone" />
       </button>
-      <Container>
+      <Container onSubmit={handleCreateNewTransaction}>
         <h2>Cadastrar transação</h2>
 
-        <input type="text" placeholder="Título" />
-        <input type="number" placeholder="Valor" />
+        <input type="text" placeholder="Título" value={title} onChange={(event) => setTitle(event.target.value)} />
+        <input type="number" placeholder="Valor" value={value} onChange={(event) => setValue(Number(event.target.value))} />
 
         <TransactionTypeContainer>
           <RadioBox
@@ -42,7 +60,7 @@ export function NewTransactionModal({ isOpen, onRequestClose }: NewTransactionMo
           ><ArrowCircleDown size={32} color="#ee5353" weight="duotone" alt="Saída" /><span>Saída</span></RadioBox>
         </TransactionTypeContainer>
 
-        <input placeholder="Categoria" />
+        <input placeholder="Categoria" value={category} onChange={event => setCategory(event.target.value)} />
         <button type="submit">Cadastrar</button>
       </Container>
     </Modal>
